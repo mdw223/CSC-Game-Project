ddc6e5e36e5054db2747ed4c59445a12c333a4c6
mdw223 <151095055+mdw223@users.noreply.github.com>
2023-11-16 18:20:39 -0500

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Game {
+
+    public static void main(String[] args) {
+
+        printMenu();
+
+    }
+
+    public static void printMenu() {
+        System.out.println(".---.---.---.---.---.---.---.---.");
+        System.out.println("|   |   |   |   |   |   |   |   |");
+        System.out.println(".---.---.---.---.---.---.---.---.");
+        System.out.println("|   |   |   |   |   |   |   |   |");
+        System.out.println(".---.---.---.---.---.---.---.---.");
+        System.out.println("|   |   |   |   |   |   |   |   |");
+        System.out.println(".---.---.---.---.---.---.---.---.");
+        System.out.println("|   |   |   |   |   |   |   |   |");
+        System.out.println(".---.---.---.---.---.---.---.---.");
+        System.out.println("|   |   |   |   |   |   |   |   |");
+        System.out.println(".---.---.---.---.---.---.---.---.");
+        System.out.println("|   |   |   |   |   |   |   |   |");
+        System.out.println(".---.---.---.---.---.---.---.---.");
+        System.out.println("|   |   |   |   |   |   |   |   |");
+        System.out.println(".---.---.---.---.---.---.---.---.");
+        System.out.println("|   |   |   |   |   |   |   |   |");
+        System.out.println(".---.---.---.---.---.---.---.---.");
+    }
+}
